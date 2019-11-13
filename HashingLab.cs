daf9917241fe6de8a55e365d7f54b593f046e05b
Duy Nguyen <48025708+duy042125@users.noreply.github.com>
2019-11-13 10:56:49 -0600

    Update HashingLab.cs

--- HashingLab.cs
+++ HashingLab.cs
@@ -1,19 +1,6 @@
 ﻿//Duy Nguyen
 //CSCI 303
 //Lab 3
-/*
- * GRADE:       27.25 / 30
- * COMMENTS:    Pretty good job.
- * 
- *              The biggest problem is in the "probe" method, when you loop until
- *              result >= 0; if result starts out as a huge negative number, this loop could run for
- *              millions of iterations! Instead, you should mod out by the table size BEFORE the loop;
- *              then the loop can become a simple "if" statement.
- *              
- *              The "isPrime" method can also be made more efficient.
- *              
- *              Search for "-sjr" to find my comments.
-*/
 
 using System;
 using System.Collections.Generic;
@@ -28,13 +15,13 @@ namespace HashingLab
         private bool linearProbing;
         private bool[] occupied;
 
-        public HashTable(int theSize, bool useLinearProbing = false)    //optional parameter useLinearProbing defaults to false (fixed)
+        public HashTable(int theSize, bool useLinearProbing = false)
         {
             linearProbing = useLinearProbing;
             if (!linearProbing) goodValue(ref theSize);
             items = new T[theSize];
             occupied = new bool[theSize];
-        }   // 3.5 / 4. See isPrime. -sjr (fixed)
+        }
 
         #region Check for good value in Size
 
@@ -57,7 +44,7 @@ namespace HashingLab
         private bool isPrime(int number)
         {
             int r = Math.Sqrt(number);
-            for (int i = 2; i <= r; i++)        //Inefficient: you only need to loop through the square root of number. -sjr (fixed)
+            for (int i = 2; i <= r; i++)
             {
                 if (number % i == 0) return false;
             }
@@ -81,7 +68,7 @@ namespace HashingLab
                 }
             }
             throw new Exception("The Hash Table is full !!!");
-        }// 6/6. -sjr
+        }
 
         private uint probe(uint index, int order)
         {
@@ -90,16 +77,14 @@ namespace HashingLab
             else
             {
                 int h = (order + 1) / 2;
-                //int result = (int)index + (int)(Math.Pow(-1, order + 1)) * (h * h); //Dangerous: Do not mix floating-point and integer arithmetic. -sjr
                 int sign = (order % 2 == 0) ? -1 : 1;
                 result = (sign*h*h) % n;
                 result = (result >= 0) ? result : result += n;
-                //while (result < 0) result += n;//This is extremely inefficient: mod out by items.Length FIRST. -sjr
                 return (uint)(result);
             }
-        }// 6/8. -sjr
+        }
 
-        public bool retrieveItem(ref T theItem)//theItem comes with its key fields filled, returns with all fields filled if found
+        public bool retrieveItem(ref T theItem)
         {
             uint key = theItem.getKey();
             uint home = hashFunction(key);
@@ -107,14 +92,14 @@ namespace HashingLab
             {
                 uint index = probe(home, i);
                 if (!occupied[index]) return false;
-                else if (items[index].getKey() == theItem.getKey()) //"occupied[index]" is redundant here. -sjr (fixed
+                else if (items[index].getKey() == theItem.getKey())
                 {
                     theItem = items[index];
                     return true;
                 }
             }
             return false;
-        }// 5.75 / 6. -sjr
+        }
 
         private uint hashFunction(uint key)
         {
@@ -127,7 +112,7 @@ namespace HashingLab
                 hashValue = (((a * b) + 8) % (uint)items.Length);
             }
             return hashValue;
-        }// 6/6. -sjr
+        }
 
         private void fold(uint key, out uint a, out uint b)
         {
@@ -136,7 +121,7 @@ namespace HashingLab
             a = (aStr.Equals("")) ? 0 : uint.Parse(aStr);
             string bStr = numStr.Substring((int)(numStr.Length / 2));
             b = (bStr.Equals("")) ? 0 : uint.Parse(bStr);
-        }//ok. -sjr
+        }
     }
 }
 
